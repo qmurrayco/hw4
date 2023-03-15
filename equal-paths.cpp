b9e1684a9209c71b1b15b85ac8299208be437708
Quinton Murray-Conn <murrayco@usc.edu>
2023-03-15 02:20:12 +0000

    Equal Paths Finished

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -10,9 +10,37 @@ using namespace std;
 // You may add any prototypes of helper functions here
 
 
-bool equalPaths(Node * root)
+bool stepCount(Node* root, int& pathVal, int lvl)
 {
-    // Add your code below
+
+	if(root == NULL){
+		//stepped one out, go back to leaf
+		return true;
+	}
+
+	if(!root->left && !root->right){
+		if(pathVal < 0){
+			//found a leaf, set it equal to the current depth
+			pathVal = lvl;
+		}
+
+		//if at any point the two arent equal, we know its uneven and will return false
+		return (pathVal == lvl);
+	}
+
+	return (stepCount(root->left, pathVal, lvl+1) && stepCount(root->right, pathVal, lvl+1));
 
 }
 
+bool equalPaths(Node * root)
+{
+    //Add your code below
+  int pathVal = -1;
+
+	//empty
+	if(root == NULL){
+		return true;
+	}
+	
+  return (stepCount(root->left, pathVal, 1) && stepCount(root->right, pathVal, 1));
+}
--- equal-paths.h
+++ equal-paths.h
@@ -29,4 +29,6 @@ struct Node {
  */
 bool equalPaths(Node * root);
 
+bool stepCount(Node* root, int& pathVal, int lvl);
+
 #endif
